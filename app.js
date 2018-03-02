859f545f785e2dbe2403dc10022d718d96d858ed
Ciaran O'Reilly <ciaran@oreilly.cat>
2018-03-02 12:35:43 +0100

    Server test

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(200);
+    res.json({
+        message: "Hello World"
+    });
+});
+
+module.exports = app;
